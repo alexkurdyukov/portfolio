4008dcdaadd787bf27874e5fbad0015f237a9408
Alexey <90500618+alexkurdyukov@users.noreply.github.com>
2022-09-22 00:34:36 +0300

    create skill table

---
+++ src/hooks/useWindowWidth.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+const useWindowWidth = () => {
+  const [windowWidth, setWindowWidth] = useState(undefined);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowWidth;
+};
+
+export { useWindowWidth };
