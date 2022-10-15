0cb37d5a44669e1ac3857f1f1c4eccc3caad36b1
Alexey <leha.kurdyukov.99@mail.ru>
2022-10-15 16:49:20 +0300

    ref

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import { scrollAvailable } from "../../scripts/scrollBlock";
 
-const Popup = ({children,setProjectOpen,projectState}) => {
+const Popup = ({ children, setProjectOpen, projectState }) => {
 	return (
 		<div className="popup">
-			<div className="project-popup wrapper">
-				{children}
-			</div>
+			<div className="project-popup wrapper">{children}</div>
 			<div
 				onClick={() => {
 					setProjectOpen(false);
@@ -18,4 +16,5 @@ const Popup = ({children,setProjectOpen,projectState}) => {
 	);
 };
 
-export {Popup} 
+export { Popup };
+
